2d37a819a814f102e7d5b3d750f1879224b46d53
pnagasaikiran <39734736+pnagasaikiran@users.noreply.github.com>
2022-04-29 01:34:03 -0700

    updated script

---
+++ script.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/bash
+
+cat  <<  EOF 
+
+██╗  ██╗ █████╗ ██╗     ██╗    ██████╗ ██████╗  ██████╗ ██╗    ██╗███████╗███████╗██████╗ 
+██║ ██╔╝██╔══██╗██║     ██║    ██╔══██╗██╔══██╗██╔═══██╗██║    ██║██╔════╝██╔════╝██╔══██╗
+█████╔╝ ███████║██║     ██║    ██████╔╝██████╔╝██║   ██║██║ █╗ ██║███████╗█████╗  ██████╔╝
+██╔═██╗ ██╔══██║██║     ██║    ██╔══██╗██╔══██╗██║   ██║██║███╗██║╚════██║██╔══╝  ██╔══██╗
+██║  ██╗██║  ██║███████╗██║    ██████╔╝██║  ██║╚██████╔╝╚███╔███╔╝███████║███████╗██║  ██║
+                   
+EOF
+
+echo "UPDATING KALI"
+sleep 5
+sudo apt update
+clear
+echo "INSTALLING NOVNC X11VNC"
+sleep 5
+sudo apt install -y novnc x11vnc
+clear
+echo "CONFIGURE X11vnc"
+sleep 5
+x11vnc -display :0 -autoport -localhost -nopw -bg -xkb -ncache -ncache_cr -quiet -forever
+ss -antp | grep vnc
+clear
+/usr/share/novnc/utils/launch.sh --listen 8081 --vnc localhost:5900
+echo "ENABLE SSH.."
+sleep 5
+sudo systemctl enable ssh --now
